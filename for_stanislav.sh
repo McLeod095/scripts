5238a4ba3446ecf0717157526bc5e70461afe33a
mcleod <mcleod095@gmail.com>
2016-04-20 14:01:46 +0300

    Он все не успокаивается

--- for_stanislav.sh
+++ for_stanislav.sh
@@ -11,32 +11,36 @@ UNLOCK="UNLOCK TABLES"
 FT=date +%Y-%m-%d_%H%M
 REMOTE_OUTPUT_DIR="/mnt/backup"
 
-if mountpoint -q -- ${REMOTE_OUTPUT_DIR} && mountpoint -q -- ${OUTPUT}; then
+if ! mountpoint -q -- ${REMOTE_OUTPUT_DIR}; then 
+	echo "${REMOTE_OUTPUT_DIR} is't mount" | mail -s "Backup error" audit@corp.net
+	exit 1
+fi 
 
-	rm ${OUTPUT_DIR}/dump.sql.gz
-	rm ${OUTPUT_DIR}/master.mysql.log
-	rm ${OUTPUT_DIR}/slave.mysql.log
-
-	#GET REPLICATION STATUSES
-	echo -e "/bin/date LOG master and slave\n" 
-	mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e"${CMD_MASTER_STATUS}" > ${OUTPUT_DIR}/master.mysql.log${FT}
-	mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e"${CMD_SLAVE_STATUS}" > ${OUTPUT_DIR}/slave.mysql.log${FT}
-	
-	#DUMPING MYSQL
-	echo -e "/bin/date dumping....\n" 
-	mysqldump —extended-insert —single-transaction —user=${MYSQL_USER} —password=${MYSQL_PASSWORD} —databases telemetry ep portal_health | gzip > ${OUTPUT_DIR}/dump${FT}.sql.gz
-	#mysqldump —extended-insert —single-transaction —user=${MYSQL_USER} —password=${MYSQL_PASSWORD} —databases ep portal_health > "${OUTPUT_DIR}/dump1${FT}.sql"
-	echo -e "/bin/date dump complete\n" 
-	
-	# Move dumps to remote server
-	cp ${OUTPUT_DIR}/dump${FT}.sql.gz       ${REMOTE_OUTPUT_DIR}/dump${FT}.sql.gz
-	cp ${OUTPUT_DIR}/master.mysql.log${FT}  ${REMOTE_OUTPUT_DIR}/master.mysql.log${FT}
-	cp ${OUTPUT_DIR}/slave.mysql.log${FT}   ${REMOTE_OUTPUT_DIR}/slave.mysql.log${FT}
-	mv ${OUTPUT_DIR}/dump${FT}.sql.gz       ${OUTPUT_DIR}/dump.sql.gz
-	mv ${OUTPUT_DIR}/master.mysql.log${FT}  ${OUTPUT_DIR}/master.mysql.log
-	mv ${OUTPUT_DIR}/slave.mysql.log${FT}   ${OUTPUT_DIR}/slave.mysql.log
-	
-	echo -e "/bin/date dump.sh finished\n"
-else
-	echo "${REMOTE_OUTPUT_DIR} is't mount" | mail -s "Dump error" audit@corp.net
+if ! mountpoint -q -- ${OUTPUT}; then
+	echo "${OUTPUT_DIR} is't mount" | mail -s "Backup error" audit@corp.net
+	exit 1
 fi
+
+rm ${OUTPUT_DIR}/dump.sql.gz
+rm ${OUTPUT_DIR}/master.mysql.log
+rm ${OUTPUT_DIR}/slave.mysql.log
+#GET REPLICATION STATUSES
+echo -e "/bin/date LOG master and slave\n" 
+mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e"${CMD_MASTER_STATUS}" > ${OUTPUT_DIR}/master.mysql.log${FT}
+mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e"${CMD_SLAVE_STATUS}" > ${OUTPUT_DIR}/slave.mysql.log${FT}
+
+#DUMPING MYSQL
+echo -e "/bin/date dumping....\n" 
+mysqldump —extended-insert —single-transaction —user=${MYSQL_USER} —password=${MYSQL_PASSWORD} —databases telemetry ep portal_health | gzip > ${OUTPUT_DIR}/dump${FT}.sql.gz
+#mysqldump —extended-insert —single-transaction —user=${MYSQL_USER} —password=${MYSQL_PASSWORD} —databases ep portal_health > "${OUTPUT_DIR}/dump1${FT}.sql"
+echo -e "/bin/date dump complete\n" 
+
+# Move dumps to remote server
+cp ${OUTPUT_DIR}/dump${FT}.sql.gz       ${REMOTE_OUTPUT_DIR}/dump${FT}.sql.gz
+cp ${OUTPUT_DIR}/master.mysql.log${FT}  ${REMOTE_OUTPUT_DIR}/master.mysql.log${FT}
+cp ${OUTPUT_DIR}/slave.mysql.log${FT}   ${REMOTE_OUTPUT_DIR}/slave.mysql.log${FT}
+mv ${OUTPUT_DIR}/dump${FT}.sql.gz       ${OUTPUT_DIR}/dump.sql.gz
+mv ${OUTPUT_DIR}/master.mysql.log${FT}  ${OUTPUT_DIR}/master.mysql.log
+mv ${OUTPUT_DIR}/slave.mysql.log${FT}   ${OUTPUT_DIR}/slave.mysql.log
+	
+echo -e "/bin/date dump.sh finished\n"
